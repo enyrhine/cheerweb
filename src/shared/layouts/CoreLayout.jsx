b026bd833e127c1b1572b6aab7abaa3b3ab4e4fc
Elsa Nyrhinen <elsa.nyrhinen@robu.fi>
2022-04-28 15:44:18 +0300

    refactor react router for critical changes

--- src/shared/layouts/CoreLayout.jsx
+++ src/shared/layouts/CoreLayout.jsx
@@ -1,64 +1,70 @@
-
-import React, { lazy } from 'react'
-import {
-    useHistory,
-    Route,
-    Switch,
-    Redirect
-} from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { useNavigate, Route, Routes } from 'react-router-dom'
 import { routerPath } from '../../constants'
 import logo from '../../assets/cheerLogo.png'
 import MenuComponent from '../components/Menu'
-
 
 const Home = lazy(() => import('../../components/Home'))
 const Team = lazy(() => import('../../components/Team'))
 
 const CoreLayout = props => {
-    const menuItems = ['Etusivu', 'Joukkueet', 'Työkalut']
-    let history = useHistory()
+  const menuItems = ['Etusivu', 'Joukkueet', 'Työkalut']
+  let navigate = useNavigate()
 
-    const goToFrontPage = () => {
-        history.push('/')
-    }
+  const goToFrontPage = () => {
+    navigate('/')
+  }
 
-    const goToTeamPage = () => {
-        history.push('/joukkue')
-    }
+  const goToTeamPage = () => {
+    navigate('/joukkue')
+  }
 
-    const goToToolsPage = () => {
-        history.push('/')
-    }
+  //   const goToToolsPage = () => {
+  //     navigate('/')
+  //   }
 
-    const menuRoutes = ['/', '/joukkue', '/']
+  const menuRoutes = ['/', '/joukkue', '/']
 
-    return (
-        <div className="app-core">
-            <header className="App-header">
-                <img src={logo} className="img-logo" alt="logo" />
-                <div className="menu-items">
-                    <p onClick={goToFrontPage}>Etusivu</p>
-                    <p onClick={goToTeamPage}>Joukkueet</p>
-                    {/* <p onClick={goToToolsPage}>Työkalut</p> */}
-                </div>
-                <MenuComponent routes={menuRoutes}>{menuItems}</MenuComponent>
-
-            </header>
-            <main>{props.children}</main>
+  return (
+    <div className="app-core">
+      <header className="App-header">
+        <img src={logo} className="img-logo" alt="logo" />
+        <div className="menu-items">
+          <p onClick={goToFrontPage}>Etusivu</p>
+          <p onClick={goToTeamPage}>Joukkueet</p>
+          {/* <p onClick={goToToolsPage}>Työkalut</p> */}
         </div>
-    )
+        <MenuComponent routes={menuRoutes}>{menuItems}</MenuComponent>
+      </header>
+      <main>{props.children}</main>
+    </div>
+  )
 }
 
 const RenderSwitchCore = () => {
-    return (
-        <CoreLayout>
-            <Switch>
-                <Route path={routerPath.team} component={Team} />
-                <Route path={routerPath.home} component={Home} />
-                <Redirect to="/" />
-            </Switch>
-        </CoreLayout>
-    )
+  return (
+    <>
+      <CoreLayout />
+      <Routes>
+        <Route
+          path={routerPath.home}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path={routerPath.team}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Team />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </>
+  )
 }
 
 export default RenderSwitchCore
