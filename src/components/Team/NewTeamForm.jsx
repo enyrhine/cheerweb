8b51e2bd5f8d5b36c49ee30e339ae23a253e21c4
Elsa Nyrhinen <elsa.nyrhinen@robu.fi>
2022-04-28 15:41:37 +0300

    refactor firebase critical changes

--- src/components/Team/NewTeamForm.jsx
+++ src/components/Team/NewTeamForm.jsx
@@ -1,100 +1,98 @@
 import React from 'react'
-import { Formik, Field } from "formik"
-import * as Yup from "yup"
-import firebase from 'firebase/app'
+import { Formik, Field } from 'formik'
+import * as Yup from 'yup'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { firebaseApp } from '../../App'
 
 const NewTeamForm = props => {
-    var db = firebase.firestore()
-    
-    const renderLevels = () => {
-        const levels = [1, 2, 3, 4, 5, 6]
-        return levels.map(level => {
-            return (<option key={level} value={level}>{level}</option>)
-        })
+  var db = getFirestore(firebaseApp)
+
+  const renderLevels = () => {
+    const levels = [1, 2, 3, 4, 5, 6]
+    return levels.map(level => {
+      return (
+        <option key={level} value={level}>
+          {level}
+        </option>
+      )
+    })
+  }
+
+  const add = async values => {
+    try {
+      const docRef = await addDoc(collection(db, 'teams'), {
+        name: values.name,
+        level: values.level,
+      })
+      // add snackbar message
+      // ADD here context for teams tms
+      console.log('Document written with ID:  ', docRef.id)
+    } catch (e) {
+      // add errormessage in snackbar
+      console.error('Error adding document: ', e)
     }
+  }
 
-    const add = values => {
-        db.collection("teams").add({
-            name: values.name,
-            level: values.level
-        })
-        .then(function(docRef) {
-            console.log("Add notification here ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
-    }
-
-    return (
-        <div className="team-form-container">
-            <Formik
-                initialValues={{ name: "" }}
-                onSubmit={async values => {
-                    await new Promise(resolve => setTimeout(resolve, 500))
-                    add(values)
-                    // alert(JSON.stringify(values, null, 2))
-                }}
-                validationSchema={Yup.object().shape({
-                    name: Yup.string()
-                        .min(3, "Joukkueen nimi tulee olla vähintään 3 kirjainta.")
-                        .required("Kenttä on pakollinen")
-                })}
-            >
-                {props => {
-                    const {
-                        values,
-                        touched,
-                        errors,
-                        dirty,
-                        isSubmitting,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        handleReset
-                    } = props;
-                    return (
-                        <form onSubmit={handleSubmit}>
-                            <label htmlFor="name" >
-                                Joukkueen nimi
-                                </label>
-                            <input
-                                id="name"
-                                placeholder="Lisää joukkueen nimi"
-                                type="text"
-                                value={values.name}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className={
-                                    errors.name && touched.name
-                                        ? "text-input error"
-                                        : "text-input"
-                                }
-                            />
-                            <label htmlFor="level" >
-                                Joukkueen level
-                                </label>
-                            <Field name="level" as="select" className="my-select">
-                                {renderLevels()}
-                            </Field>
-                            <button
-                                type="button"
-                                className="outline"
-                                onClick={handleReset}
-                                disabled={!dirty || isSubmitting}
-                            >
-                                Tyhjennä
-                            </button>
-                            <button type="submit" disabled={isSubmitting}>
-                                Tallenna
-                             </button>
-
-                        </form>
-                    )
-                }}
-            </Formik>
-        </div>
-    )
+  return (
+    <div className="team-form-container">
+      <Formik
+        initialValues={{ name: '', level: 1 }}
+        onSubmit={values => add(values)}
+        validationSchema={Yup.object().shape({
+          name: Yup.string()
+            .min(3, 'Joukkueen nimi tulee olla vähintään 3 kirjainta.')
+            .required('Kenttä on pakollinen'),
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+          } = props
+          return (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="name">Joukkueen nimi</label>
+              <input
+                id="name"
+                placeholder="Lisää joukkueen nimi"
+                type="text"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.name && touched.name
+                    ? 'text-input error'
+                    : 'text-input'
+                }
+              />
+              <label htmlFor="level">Joukkueen level</label>
+              <Field name="level" as="select" className="my-select">
+                {renderLevels()}
+              </Field>
+              <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+              >
+                Tyhjennä
+              </button>
+              <button type="submit" disabled={isSubmitting}>
+                Tallenna
+              </button>
+            </form>
+          )
+        }}
+      </Formik>
+    </div>
+  )
 }
 
-export default (NewTeamForm)
+export default NewTeamForm
--- src/components/Team/Teams.jsx
+++ src/components/Team/Teams.jsx
@@ -1,36 +1,29 @@
-
 import React, { useState, useEffect } from 'react'
-import firebase from 'firebase/app'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { firebaseApp } from '../../App'
 
 const Teams = () => {
-    const [teams, setTeams] = useState([])
+  const [teams, setTeams] = useState([])
 
-    const db = firebase.firestore()
+  const db = getFirestore(firebaseApp)
 
-    useEffect(() => {
-        getTeams()
-            .then(res => setTeams(res))
-    }, [])
+  const getTeams = async () => {
+    const snapshot = await getDocs(collection(db, 'teams'))
+    console.log(snapshot)
+    return snapshot.docs
+  }
 
-    const getTeams = async () => {
-        const snapshot = await db.collection('teams').get()
-        return snapshot.docs
-    }
+  useEffect(() => {
+    getTeams().then(res => setTeams(res))
+  }, [])
 
-    const renderTeams = () => {
-        return (teams.map(team => {
-            return (
-                <p key={team.id}>{`${team.data().name} ${team.data().level}`}</p>
-            )
-        })
-        )
-    }
+  const renderTeams = () => {
+    return teams.map(team => {
+      return <p key={team.id}>{`${team.data().name} ${team.data().level}`}</p>
+    })
+  }
 
-    return (
-        <div>
-            {renderTeams()}
-        </div>
-    )
+  return <div>{renderTeams()}</div>
 }
 
-export default (Teams)
+export default Teams
