1e5b53e0d968cd081211fc857951158b9790f012
Elsa Nyrhinen <elsa.nyrhinen@robu.fi>
2022-04-28 15:40:13 +0300

    refactor code more prettier

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,27 +1,20 @@
-/**
- * Created by Elsa
- */
-
 import React from 'react'
 import logo from '../../assets/cheerLogo.png'
 
 const Home = props => {
-
-
-    return (
-        <div className="home">
-            <div className="home-title">
-                <h1>Cheerapp <br/> suunnittele <br/> oma joukkueesi</h1>
-                <img src={logo} alt="logo-background" />
-            </div>
-            <div className="home-box">
-            </div>
-            <div className="home-box-second">
-            </div>
-            <div className="home-box-third">
-            </div>
-        </div>
-    )
+  return (
+    <div className="home">
+      <div className="home-title">
+        <h1>
+          Cheerapp <br /> kaikki <br /> kilpailutuloksistasi
+        </h1>
+        <img src={logo} alt="logo-background" />
+      </div>
+      <div className="home-box"></div>
+      <div className="home-box-second"></div>
+      <div className="home-box-third"></div>
+    </div>
+  )
 }
 
-export default (Home)
+export default Home
--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,19 +1,18 @@
-
 import React from 'react'
 import NewTeamForm from './NewTeamForm'
 import Teams from './Teams'
 
 const Team = props => {
-    return (
-        <div>
-            <div className="team-container">
-                <h1>Lisää uusi joukkue</h1>
-                <NewTeamForm />
-                <h1>Joukkueet</h1>
-                <Teams/>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="team-container">
+        <h1>Lisää uusi joukkue</h1>
+        <NewTeamForm />
+        <h1>Joukkueet</h1>
+        <Teams />
+      </div>
+    </div>
+  )
 }
 
-export default (Team)
+export default Team
