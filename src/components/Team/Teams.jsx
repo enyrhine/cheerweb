1277a909a975a4e7d24aca12986c0108337cc856
enyrhine <elsa.nyrhinen@gmail.com>
2022-09-05 11:08:49 +0300

    refactor teams fetching to useCallback function

--- src/components/Team/Teams.jsx
+++ src/components/Team/Teams.jsx
@@ -1,28 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { collection, getDocs, getFirestore } from 'firebase/firestore'
 import { firebaseApp } from '../../App'
 import { useTeams } from '../../contexts/teamContext'
 
 const Teams = () => {
-  const [teams, setTeams] = useTeams()
+  const [teams, setTeams] = useTeams([])
+
+  const fetchTeams = useCallback(() => {
+      // const getTeams = ()
+    const db = getFirestore(firebaseApp)
+    return getDocs(collection(db, 'teams'))
+  }, [])
 
   useEffect(() => {
-    const db = getFirestore(firebaseApp)
-
     const getTeams = async () => {
       try {
-        const snapshot = await getDocs(collection(db, 'teams'))
-        setTeams(snapshot.docs)
+        const snapshot = await fetchTeams()
+        const array = []
+        snapshot.docs.map(team => {
+          return array.push({
+            id: team.id,
+            name: team.data().name,
+            level: team.data().level,
+          })
+        })
+        setTeams(array)
       } catch (e) {
         console.error('Error getting teams data: ', e)
       }
     }
+
     getTeams()
-  }, [])
+  }, [fetchTeams, setTeams])
 
   const renderTeams = () => {
     return teams.map(team => {
-      return <p key={team.id}>{`${team.data().name} ${team.data().level}`}</p>
+      return <p key={team.id}>{`${team.name} ${team.level}`}</p>
     })
   }
 
